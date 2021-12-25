5593a218b67c5f5d13ca7995be6d45db161d8eca
Karey Higuera <karey.higuera@gmail.com>
2021-12-25 08:37:38 -0600

    adjust multipass sizing for mobile phone sizes

--- components/multipass.tsx
+++ components/multipass.tsx
@@ -33,27 +33,27 @@ export const Multipass = () => {
       {isUserLoading && <h2>Loading...</h2>}
       {!isUserLoading && (
         <>
-          <div className="bg-white shadow-md overflow-hidden sm:rounded-lg mt-5">
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg text-left">
             <div className="px-4 py-5 sm:px-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">API Access Information</h3>
-              <p className="mt-1 text-sm text-gray-500">Your API key and other information about your API usage.</p>
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">Your API key and other information about your API usage.</p>
             </div>
-            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-              <dl className="sm:divide-y sm:divide-gray-200">
-                <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <div className="border-t border-gray-200">
+              <dl>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                   <dt className="text-sm font-medium text-gray-500">API Key</dt>
-                  <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex">
                     {keyState === 'ready' && (
                       <>
                         <span className="flex-grow">{user?.key ?? 'No key'}</span>
-                        <span className="ml-4 flex-shrink-0">
+                        <span className="sm:ml-4 flex-shrink-0">
                           <button type="button" onClick={() => generateOwnKey()} className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
                             Generate new key
                           </button>
                         </span>
                       </>
                     )}
-                    {keyState === 'error' && <span className="flex-grow text-red-900">There was an error generating your API key</span>}
+                    {keyState === 'error' && <span className="flex-grow text-red-900">Error</span>}
                     {keyState === 'loading' && (
                       <span className="flex-grow flex justify-center items-center">
                         <KeyIcon className="h-4 w-auto animate-wiggle text-emerald-600" />
@@ -61,10 +61,10 @@ export const Multipass = () => {
                     )}
                   </dd>
                 </div>
-                {/* <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Tweets in last 30 days</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">1123 tweets</dd>
-          </div> */}
+                {/* <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">Application for</dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
+                </div> */}
               </dl>
             </div>
           </div>
--- pages/account.tsx
+++ pages/account.tsx
@@ -14,7 +14,7 @@ const Account: NextPage = () => {
         <link rel="preload" href="/fonts/Cartridge-Regular.woff2" as="font" crossOrigin="" />
       </Head>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md md:max-w-4xl flex flex-col items-center text-center text-slate-700 text-lg px-4 sm:px-6">
+        <div className="sm:mx-auto sm:w-full sm:max-w-lg md:max-w-4xl flex flex-col items-center text-center text-slate-700 text-lg px-4 sm:px-6">
           <h2 className="font-header text-6xl text-slate-800">Account</h2>
           <ClientOnly>
             {user && (
