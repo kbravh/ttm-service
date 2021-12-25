68885e4354af3bccd595a5f46395f94304a2e24e
Karey Higuera <karey.higuera@gmail.com>
2021-12-25 10:13:02 -0600

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
+
 export const Footer = () => {
   return (
-    <footer>
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
-          <span className="block sm:inline">&copy; Karey Higuera.</span> <span className="block sm:inline">All rights reserved.</span>
+    <footer className="w-full mt-6 border-t border-gray-200">
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-5xl">
+        <div className="py-8 text-sm text-gray-500 text-center sm:text-left flex flex-col justify-between sm:flex-row">
+          <div className="order-last sm:order-none sm:flex sm:items-center mt-3 sm:mt-0">
+            <span className="block sm:inline">&copy; 2021-present Karey Higuera.</span> <span className="block sm:inline">All rights reserved.</span>
+          </div>
+          <div className="flex flex-col">
+            <h5>Links</h5>
+            <Link href="/privacy-policy">
+              <a className="font-semibold hover:underline hover:decoration-wavy hover:decoration-emerald-400">Privacy Policy</a>
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import UserProvider from '../context/user'
 import { Header } from '../components/header'
+import { Footer } from '../components/footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Header />
-      <Component {...pageProps} />
+      <div>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </UserProvider>
   )
 }
