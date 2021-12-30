f3b5ca998e7e4d951ebc8e906c9fb44c7e5eaf9a
Karey Higuera <karey.higuera@gmail.com>
2021-12-30 11:50:09 -0600

    add loading state for login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        header: ['Cartridge']
+        header: ['Cartridge'],
       },
       keyframes: {
         key_wiggle: {
           '0%, 100%': { transform: 'rotate(70deg)' },
           '50%': { transform: 'rotate(10deg)' },
         },
+        gear: {
+          '100%': { transform: 'rotate(360deg)' },
+        },
       },
       animation: {
         key_wiggle: 'key_wiggle 3s ease-in-out infinite',
-      }
-    }
+        gear: 'gear 10s infinite linear',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
