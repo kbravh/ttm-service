6452762884a80374f019f0fa06fa53e3d55899d0
Karey Higuera <karey.higuera@gmail.com>
2021-12-30 14:13:32 -0600

    adjust alignment on obsidian link again

--- components/obsidianLink.tsx
+++ components/obsidianLink.tsx
@@ -1,8 +1,8 @@
 export const ObsidianLink = () => {
   return (
     <span className="inline-block -ml-1">
-      <a href="https://obsidian.md" className="no-underline flex items-center group">
-        <span>
+      <a href="https://obsidian.md" className="no-underline group">
+        <div className="flex items-baseline">
           <svg viewBox="0 0 63 100" width="18.9" height="15" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <linearGradient id="a" x1="82.85" y1="30.41" x2="51.26" y2="105.9" gradientTransform="matrix(1 0 0 -1 -22.41 110.97)" gradientUnits="userSpaceOnUse">
@@ -15,8 +15,8 @@ export const ObsidianLink = () => {
             <path fill="#af9ff4" d="M63 26.39 44.61 0l-1.17 14.41Z" />
             <path fill="#4a37a0" d="M43.44 14.41 44.61 0l-31.7 17.52 3.52 18.18ZM16.43 35.7l3.14 54.77L47.35 100Z" />
           </svg>
-        </span>
         <span className="no-underline group-hover:underline group-hover:decoration-dotted text-purple-900 group-hover:decoration-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 rounded-full">Obsidian</span>
+        </div>
       </a>
     </span>
   );
