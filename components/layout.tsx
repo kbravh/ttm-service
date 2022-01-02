44876866aea30a371421161b377a807caefa9d27
Karey Higuera <karey.higuera@gmail.com>
2022-01-01 21:05:30 -0600

    updated dev environment values

--- components/layout.tsx
+++ components/layout.tsx
@@ -30,10 +30,16 @@ export const Layout: FC<Props> = ({ children, title, description }) => {
         <meta property="og:image" content="https://ttm.kbravh.dev/ttm_social.png?1" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="628" />
-        <meta property="og:description" content={description ?? "Save tweets as beautiful Markdown. Tweet to Markdown helps you archive the knowledge and insights you find on Twitter. Build up your personal knowledge base and avoid losing information in the ephemeral internet."} />
+        <meta
+          property="og:description"
+          content={description ?? 'Save tweets as beautiful Markdown. Tweet to Markdown helps you archive the knowledge and insights you find on Twitter. Build up your personal knowledge base and avoid losing information in the ephemeral internet.'}
+        />
         <meta property="og:locale" content="en_US" />
         <meta property="twitter:title" content={`Tweet to Markdown${title ? ` | ${title}` : ''}`} />
-        <meta property="twitter:description" content={description ?? "Save tweets as beautiful Markdown. Tweet to Markdown helps you archive the knowledge and insights you find on Twitter. Build up your personal knowledge base and avoid losing information in the ephemeral internet."} />
+        <meta
+          property="twitter:description"
+          content={description ?? 'Save tweets as beautiful Markdown. Tweet to Markdown helps you archive the knowledge and insights you find on Twitter. Build up your personal knowledge base and avoid losing information in the ephemeral internet.'}
+        />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:creator" content="@kbravh" />
         <meta property="twitter:image" content="https://ttm.kbravh.dev/ttm_social.png?1" />
