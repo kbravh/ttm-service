fca029522731a9aa4106894b8e8698829d227a74
Karey Higuera <karey.higuera@gmail.com>
2022-01-02 18:25:37 -0600

    adjust usage endpoint to allow for a client user to request

--- pages/api/usage.ts
+++ pages/api/usage.ts
@@ -4,16 +4,22 @@ import { UserProfile } from '../../types/database';
 
 const adminSupabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.PRIVATE_SUPABASE_KEY ?? '');
 
-/**
- * This API is called as a function hook any time a new user record is created.
- */
 const handler: NextApiHandler = async (req, res): Promise<void> => {
-  if (req.query.SECRET !== process.env.API_SECRET_KEY) {
-    return res.status(401).send('You are not authorized to use this API');
-  }
+  let userId = '';
 
-  if (!req.body.user) {
-    return res.status(400).send("You must include the user's ID");
+  const { user: actingUser } = await adminSupabase.auth.api.getUserByCookie(req);
+
+  //if a user isn't making the request, check for admin key
+  if (actingUser?.id) {
+    userId = actingUser.id;
+  } else {
+    if (req.query.SECRET !== process.env.API_SECRET_KEY) {
+      return res.status(401).send('You are not authorized to use this API');
+    }
+    if (!req.body.user) {
+      return res.status(400).send("You must include the user's ID");
+    }
+    userId = req.body.user;
   }
 
   const subscriptionRequest = adminSupabase.from<UserProfile>('users').select('subscriptions:subscription_id (*)').eq('id', req.body.user).single();
