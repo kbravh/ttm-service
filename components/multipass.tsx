e20dfe4c43e55e89a512ba3bdd6a9cfee9988fa7
Karey Higuera <karey.higuera@gmail.com>
2022-01-11 21:05:53 -0600

    update wording on tweet usage

--- components/multipass.tsx
+++ components/multipass.tsx
@@ -113,7 +113,7 @@ export const Multipass = () => {
                 {usage && (
                   <>
                     <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt className="text-sm font-medium text-gray-500">Downloads used</dt>
+                      <dt className="text-sm font-medium text-gray-500">Tweets used</dt>
                       <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex">
                         <span className='flex-grow'>
                           {usage?.used}/{usage?.limit}
