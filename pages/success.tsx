d0d939219f78473758106e097ce1ad893bcd719d
Karey Higuera <karey.higuera@gmail.com>
2022-05-19 21:19:31 -0500

    extract portal creation function

--- pages/success.tsx
+++ pages/success.tsx
@@ -3,23 +3,13 @@ import Link from 'next/link'
 import { Layout } from '../components/layout'
 import { MainWrapper } from '../components/mainWrapper'
 import { CashIcon, UserIcon } from '@heroicons/react/outline'
-import axios from 'axios'
 import { useState } from 'react'
+import { handlePortalClick } from '../utils/redirects'
 
 const Success: NextPage = () => {
-  const [error, setError] = useState(false);
-
-  const handlePortalClick = async () => {
-    try {
-      const response = await axios('/api/portal', {
-        method: 'GET'
-      })
-      const {url} = response.data as {url: string}
-      window.location.replace(url)
-    } catch (error) {
-      setError(true)
-    }
-  }
+  const [portalState, setPortalState] = useState<'ready' | 'loading' | 'error'>(
+    'ready'
+  )
 
   return (
     <Layout title="Success">
@@ -44,7 +34,9 @@ const Success: NextPage = () => {
             <div className="absolute inset-0 rounded-full blur bg-gradient-to-tr from-emerald-400 to-indigo-500 opacity-75 transition group-hover:opacity-100 group-hover:scale-105 group-hover:duration-200 duration-1000"></div>
             <button
               className="relative block px-5 py-2 rounded-md bg-slate-800 leading-none text-slate-100 font-semibold"
-              onClick={handlePortalClick}
+              onClick={() =>
+                handlePortalClick((state) => setPortalState(state))
+              }
             >
               <span className="flex items-center space-x-2">
                 <CashIcon className="h-5 w-auto" />
---
+++ utils/redirects.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const handlePortalClick = async (
+  stateHandler: (state: 'ready' | 'loading' | 'error') => void
+) => {
+  stateHandler('loading')
+  try {
+    const response = await axios('/api/portal', {
+      method: 'GET',
+    })
+    const { url } = response.data as { url: string }
+    window.location.replace(url)
+  } catch (error) {
+    stateHandler('error')
+  }
+}
