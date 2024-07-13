3a6f1c155e6aa43ce75500b77598774368152146
Karey Higuera <karey.higuera@gmail.com>
2024-07-13 00:10:43 -0400

    rip out user provider and sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,25 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import UserProvider from '../context/user';
-import { Header } from '../components/header';
-import { Footer } from '../components/footer';
-import { AnimatePresence } from 'framer-motion';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Header } from '../components/header'
+import { Footer } from '../components/footer'
+import { AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <UserProvider>
+    <>
       <div>
         <Header />
-        <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </div>
       <Footer />
-    </UserProvider>
-  );
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
